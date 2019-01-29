a1dd90df810f1e3a977144be5907970264bee188
Günter Neumann <neumann@dfki.de>
2019-01-29 16:27:39 +0100

    changing tests

--- src/main/java/de/dfki/lt/nemex/Test_NemexF.java
+++ src/main/java/de/dfki/lt/nemex/Test_NemexF.java
@@ -18,21 +18,21 @@ public class Test_NemexF {
 		
 		// test Faerie Paper
 
-				nemexFBean.setnGramSize(1);
-				nemexFBean.setGazetteerFilePath("src/main/webapp/resources/faerie.txt");
-		
-				nemexFBean.setSimilarityMeasure(SimilarityMeasure.ED_SIMILARITY_MEASURE);
-				nemexFBean.setSimilarityThreshold(0.0);
-		
-				nemexFBean.setQueryString("an efficient filter for approximate membership checking. venkaee "
-						+ "shga kamunshik kabarati, dong xin, surauijt chadhurisigmod.");
+//				nemexFBean.setnGramSize(1);
+//				nemexFBean.setGazetteerFilePath("src/main/webapp/resources/faerie.txt");
+//		
+//				nemexFBean.setSimilarityMeasure(SimilarityMeasure.ED_SIMILARITY_MEASURE);
+//				nemexFBean.setSimilarityThreshold(0.0);
+//		
+//				nemexFBean.setQueryString("an efficient filter for approximate membership checking. venkaee "
+//						+ "shga kamunshik kabarati, dong xin, surauijt chadhurisigmod.");
 		
 				//nemexFBean.setQueryString("chakrabarti");
 		
 				//nemexFBean.setQueryString("venkaee shga kamunshi");
 
 		// test with examples from:
-//		nemexFBean.setnGramSize(3);
+		nemexFBean.setnGramSize(5);
 		
 
 //		nemexFBean.setSimilarityMeasure(SimilarityMeasure.COSINE_SIMILARITY_MEASURE);
@@ -51,30 +51,33 @@ public class Test_NemexF {
 //						+ "cytochemical MPO expression in acute leukemia does not seem to require myeloid leukemia treatment in case of "
 //						+ "otherwise lymphoblastic cytomorphology and phenotype."
 //						);
-//				
+				
 //				nemexFBean.setQueryString("lymfoblastic");
 
 //		// NE-List:
+		nemexFBean.setnGramSize(3);
+		nemexFBean.setSimilarityMeasure(SimilarityMeasure.COSINE_SIMILARITY_MEASURE);
+		nemexFBean.setSimilarityThreshold(0.8);
+		
 //		nemexFBean.setnGramSize(3);
-
-//		// "/Users/gune00/data/NE-Lists/CrossNER/all.txt";
+//		nemexFBean.setSimilarityMeasure(SimilarityMeasure.ED_SIMILARITY_MEASURE);
+//		nemexFBean.setSimilarityThreshold(1);
+		
 //		// "/Users/gune00/data/NE-Lists/pantelWikiListSeeds.txt"
-//		nemexFBean.setSimilarityMeasure(SimilarityMeasure.COSINE_SIMILARITY_MEASURE);
-//		nemexFBean.setSimilarityThreshold(0.8);
-//		nemexFBean.setGazetteerFilePath("/Users/gune00/data/NE-Lists/CrossNER/all.txt");
-//		nemexFBean.setQueryString(
-//				"Blackrock is a 1997 Australian drama film directed by Steven Vidler and written by Nick Enright. "
-//				+ "In Blackrock, a fictional beachside working-class suburb, a young surfer witnesses his friends raping a girl. "
-//				+ "When she is found murdered the next day, he is torn between revealing what he saw and protecting his friends. "
-//				+ "Filming locations included Stockton, New South Wales, where a girl named Leigh Leigh was murdered in 1989. "
-//				+ "While the film was never marketed as the story of her death, many viewers incorrectly believed it to "
-//				+ "be a factual account of the crime. Her family objected to what they saw as a fictionalisation of her murder, "
-//				+ "and residents of Stockton opposed the decision to shoot scenes there. The film received generally positive critical "
-//				+ "reviews in Australia, where it was nominated for five AACTA Awards and won two AWGIE Awards, "
-//				+ "though it received mixed reviews elsewhere. Adapted from Enright's play of the same name, "
-//				+ "the film stars Laurence Breuls, Simon Lyndon and Linda Cropper, "
-//				+ "and features the first credited film performance of Heath Ledger. "
-//			);
+		nemexFBean.setGazetteerFilePath("/Users/gune00/data/NE-Lists/CrossNER/all.txt");
+		nemexFBean.setQueryString(
+				"Blackrock is a 1997 Australian drama film directed by Steven Vidler and written by Nick Enright. "
+				+ "In Blackrock, a fictional beachside working-class suburb, a young surfer witnesses his friends raping a girl. "
+				+ "When she is found murdered the next day, he is torn between revealing what he saw and protecting his friends. "
+				+ "Filming locations included Stockton, New South Wales, where a girl named Leigh Leigh was murdered in 1989. "
+				+ "While the film was never marketed as the story of her death, many viewers incorrectly believed it to "
+				+ "be a factual account of the crime. Her family objected to what they saw as a fictionalisation of her murder, "
+				+ "and residents of Stockton opposed the decision to shoot scenes there. The film received generally positive critical "
+				+ "reviews in Australia, where it was nominated for five AACTA Awards and won two AWGIE Awards, "
+				+ "though it received mixed reviews elsewhere. Adapted from Enright's play of the same name, "
+				+ "the film stars Laurence Breuls, Simon Lyndon and Linda Cropper, "
+				+ "and features the first credited film performance of Heath Ledger. "
+			);
 //		
 		// END of parameter setting
 		
